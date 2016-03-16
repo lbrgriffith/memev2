dda8196055dce83a0e4bbcb8c827a91dd8d93b9c
Ricardo Griffith <ricardogriffith@icloud.com>
2016-03-16 20:55:08 -0300

    Added an extension to the MeMe struct in order to use the NSCoding class to save the struct to file.

--- meme_ex_1/Meme.swift
+++ meme_ex_1/Meme.swift
@@ -17,4 +17,54 @@ struct Meme {
     var OriginalImage : UIImage
     // a memed image, combining the text and the original image
     var MemeImage : UIImage
+    
+    static func encode(meme: Meme) {
+        let memeClassObject = MemeHelperClass(meme: meme)
+        
+        NSKeyedArchiver.archiveRootObject(memeClassObject, toFile: MemeHelperClass.path())
+    }
+    
+    static func decode() -> Meme? {
+        let memeClassObject = NSKeyedUnarchiver.unarchiveObjectWithFile(MemeHelperClass.path()) as? MemeHelperClass
+        
+        return memeClassObject?.meme
+    }
+}
+
+// Adds a class within the Meme struct that conforms to 
+// NSCoding as the data persistence mechanism.
+extension Meme {
+    class MemeHelperClass: NSObject, NSCoding {
+        
+        var meme: Meme?
+        
+        init(meme: Meme) {
+            self.meme = meme
+            super.init()
+        }
+        
+        class func path() -> String {
+            let documentsPath = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true).first
+            let path = documentsPath?.stringByAppendingString("/savedMemes")
+            return path!
+        }
+        
+        required init?(coder aDecoder: NSCoder) {
+            guard let topString = aDecoder.decodeObjectForKey("topString") as? String else { meme = nil; super.init(); return nil }
+            guard let bottomString = aDecoder.decodeObjectForKey("bottomString") as? String else { meme = nil; super.init(); return nil }
+            guard let originalImage = aDecoder.decodeObjectForKey("originalImage") as? UIImage else { meme = nil; super.init(); return nil }
+            guard let memeImage = aDecoder.decodeObjectForKey("memeImage") as? UIImage else { meme = nil; super.init(); return nil }
+            
+            meme = Meme(topString: topString, bottomString: bottomString, OriginalImage: originalImage, MemeImage: memeImage)
+            
+            super.init()
+        }
+        
+        func encodeWithCoder(aCoder: NSCoder) {
+            aCoder.encodeObject(meme!.topString, forKey: "topString")
+            aCoder.encodeObject(meme!.bottomString, forKey: "bottomString")
+            aCoder.encodeObject(meme!.OriginalImage, forKey: "originalImage")
+            aCoder.encodeObject(meme!.MemeImage, forKey: "memeImage")
+        }
+    }
 }
